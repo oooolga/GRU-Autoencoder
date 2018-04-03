fc58a8485a031b5c61014f43acd56c1681cb7874
oooolga <olga.xu823@gmail.com>
2018-04-03 00:18:43 -0400

    commit change model

--- model/LSTMModel.py
+++ model/LSTMModel.py
@@ -19,6 +19,16 @@ class LSTMAutoEncoder(nn.Module):
 		self.decoder = nn.GRU(self.hidden_size, self.input_size,
 							  1, batch_first=True)
 		self.out_layer = nn.Linear(self.hidden_size*sequence_len, self.num_class)
+		self.encoded_h0 = nn.Parameter(torch.zeros(self.num_layers, batch_size, self.hidden_size))
+		self.decoded_h0 = nn.Parameter(torch.zeros(1, batch_size, self.input_size))
+
+		self.idx = [i for i in range(x.size(1)-1, -1, -1)]
+		self.idx = Variable(torch.LongTensor(self.idx))
+		
+		if torch.cuda.is_available():
+			self.encoded_h0 = self.encoded_h0.cuda()
+			self.decoded_h0 = self.decoded_h0.cuda()
+			self.idx = self.idx.cuda()
 
 		self.gamma = gamma
 
@@ -29,21 +39,12 @@ class LSTMAutoEncoder(nn.Module):
 	def forward(self, x):
 
 		batch_size = x.size(0)
-		encoded_h0 = nn.Parameter(torch.zeros(self.num_layers, batch_size, self.hidden_size))
-		decoded_h0 = nn.Parameter(torch.zeros(1, batch_size, self.input_size))
-		idx = [i for i in range(x.size(1)-1, -1, -1)]
-		idx = Variable(torch.LongTensor(idx))
-
-		if torch.cuda.is_available():
-			encoded_h0 = encoded_h0.cuda()
-			decoded_h0 = decoded_h0.cuda()
-			idx = idx.cuda()
-
-		encoded_h, encoded_hn = self.encoder(x, encoded_h0)
+		
+		encoded_h, encoded_hn = self.encoder(x, self.encoded_h0)
 		out = F.log_softmax(self.out_layer(encoded_h.contiguous().view(batch_size, -1)))
 
-		encoded_h = encoded_h.index_select(1, idx)
-		decoded_h, decoded_hn = self.decoder(encoded_h, decoded_h0)
+		encoded_h = encoded_h.index_select(1, self.idx)
+		decoded_h, decoded_hn = self.decoder(encoded_h, self.decoded_h0)
 		return out, decoded_h
 
 
